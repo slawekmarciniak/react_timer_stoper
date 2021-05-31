588e0f4176535d8d081dd55af5043ff9475c7c6b
Slawek <61154596+Ultran@users.noreply.github.com>
2021-05-31 21:44:53 +0200

    first commit

---
+++ app.js
@@ -0,0 +1,207 @@
+class Clock extends React.Component {
+  state = {
+    time: this.getTime(),
+  };
+
+  getTime() {
+    const time = new Date();
+    return {
+      seconds: time.getSeconds(),
+      minutes: time.getMinutes(),
+      hours: time.getHours(),
+    };
+  }
+
+  changeTime = () => {
+    this.setState({
+      time: this.getTime(),
+    });
+  };
+
+  componentDidMount = () => {
+    this.interval = setInterval(this.changeTime, 1000);
+  };
+
+  componentWillUnmount() {
+    clearInterval(this.interval);
+  }
+
+  render() {
+    const { minutes, seconds, hours } = this.state.time;
+    return (
+      <p>
+        {hours > 9 ? hours : `0${hours}`} :{" "}
+        {minutes > 9 ? minutes : `0${minutes}`} :{" "}
+        {seconds > 9 ? seconds : `0${seconds}`}
+      </p>
+    );
+  }
+}
+
+class Stoper extends React.Component {
+  state = {
+    seconds: 0,
+    intervalOn: false,
+  };
+
+  componentWillUnmount = () => {
+    clearInterval(this.interval);
+  };
+
+  start = () => {
+    if (this.interval) {
+      clearInterval(this.interval);
+    }
+    this.interval = setInterval(() => {
+      this.setState({
+        seconds: this.state.seconds + 1,
+      });
+    }, 1000);
+    this.setState({
+      intervalOn: true,
+    });
+  };
+
+  pause = () => {
+    clearInterval(this.interval);
+  };
+
+  reset = () => {
+    clearInterval(this.interval);
+    this.setState({
+      seconds: 0,
+    });
+  };
+
+  render() {
+    return (
+      <>
+        <p>{this.state.seconds}</p>
+        <button onClick={this.start}>start</button>
+        <button onClick={this.pause}>pause</button>
+        <button onClick={this.reset}>reset</button>
+      </>
+    );
+  }
+}
+
+class Timer extends React.Component {
+  state = {
+    minutes: 0,
+    seconds: 0,
+  };
+
+  handleMinutes = (e) => {
+    if (e.target.value < 0) {
+      alert("please dont use negative numbers ");
+    }
+    this.setState({
+      minutes: e.target.value,
+    });
+  };
+
+  handleSeconds = (e) => {
+    if (e.target.value < 0) {
+      alert("please dont use negative numbers ");
+    }
+
+    this.setState({
+      seconds: e.target.value,
+    });
+  };
+
+  handleStart = () => {
+    if (this.state.minutes > 0 || this.state.seconds > 0) {
+      this.interval = setInterval(() => {
+        console.log("start timer interval");
+        this.setState({
+          seconds: this.state.seconds - 1,
+        });
+      }, 1000);
+    }
+  };
+
+  handlePause = () => {
+    clearInterval(this.interval);
+  };
+
+  handleReset = () => {
+    if (this.interval) {
+      clearInterval(this.interval);
+    }
+    this.setState({
+      minutes: 0,
+      seconds: 0,
+    });
+  };
+
+  componentDidUpdate = () => {
+    if (this.state.minutes === 0 && this.state.seconds === 0 && this.interval) {
+      
+      clearInterval(this.interval);
+    } else if (
+      this.state.seconds === 0 &&
+      this.state.minutes > 0 &&
+      this.interval
+    ) {
+      this.setState({
+        minutes: this.state.minutes - 1,
+        seconds: 60,
+      });
+    }
+  };
+
+  componentWillUnmount = () => {
+    clearInterval(this.interval);
+  };
+
+  render() {
+    return (
+      <>
+        <p>minutes</p>
+        <input
+          onChange={this.handleMinutes}
+          type="number"
+          value={this.state.minutes}
+        />
+        <p>seconds</p>
+        <input
+          onChange={this.handleSeconds}
+          type="number"
+          value={this.state.seconds}
+        />{" "}
+        <br />
+        <button onClick={this.handleStart}>start</button>
+        <button onClick={this.handlePause}>pause</button>
+        <button onClick={this.handleReset}>reset</button>
+      </>
+    );
+  }
+}
+
+class App extends React.Component {
+  state = {
+    type: "",
+  };
+
+  changeType = (type) => {
+    this.setState({
+      type,
+    });
+  };
+  render() {
+    // const { type } = this.state;
+    return (
+      <>
+        <button onClick={() => this.changeType("clock")}>Clock</button>
+        <button onClick={() => this.changeType("stoper")}>Stoper</button>
+        <button onClick={() => this.changeType("timer")}>Timer</button> <br />
+        {this.state.type === "clock" ? <Clock /> : null}
+        {this.state.type === "stoper" ? <Stoper /> : null}
+        {this.state.type === "timer" ? <Timer /> : null}
+      </>
+    );
+  }
+}
+
+ReactDOM.render(<App />, document.getElementById("root"));
